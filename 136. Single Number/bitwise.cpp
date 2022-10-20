72246dc771ba2487fac744be4e1a44eaff62657c
Jackson <56949000+jacksonchen1998@users.noreply.github.com>
2022-10-20 21:25:50 +0800

    Create bitwise.cpp

---
+++ 136. Single Number/bitwise.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for (auto i : nums)
+            result ^= i;
+        return result;
+    }
+};      
