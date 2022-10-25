bf214a51d7fac5dc76faaef3f71e28006e527404
Jackson <56949000+jacksonchen1998@users.noreply.github.com>
2022-10-25 19:26:41 +0800

    [UPDATE] Add 4 kind of solutions

---
+++ 342. Power of Four/bitwise.py
@@ -0,0 +1,15 @@
+class Solution:
+  def isPowerOfFour(self, n : int) -> bool:
+    return n > 0 and n % 3 == 1 and n & n - 1 == 0
+  
+class Solution:
+  def isPowerOfFour(self, n : int) -> bool:
+    return n > 0 and n & n - 1 == 0 and n & 0xaaaaaaaa == 0
+  
+class Solution:
+  def isPowerOfFour(self, n : int) -> bool:
+    return n > 0 and n & n - 1 == 0 and int(sqrt(n)) * int(sqrt(n)) == n
+  
+class Solution:
+  def isPowerOfFour(self, n : int) -> bool:
+    retuin n > 0 and log(n, 4).is_integer()
