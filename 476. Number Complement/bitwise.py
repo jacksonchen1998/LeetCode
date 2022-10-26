9d265014e68b97021107fac315e18b24073fcd69
Jackson <56949000+jacksonchen1998@users.noreply.github.com>
2022-10-26 15:58:39 +0800

    [UPDATE] Add Python 2 version

--- 476. Number Complement/bitwise.py
+++ 476. Number Complement/bitwise.py
@@ -1,5 +1,23 @@
+# Python 3
+
 class Solution:
   def findComplement(self, num : int) -> int:
+    self.x = num
+    num |= num >> 1
+    num |= num >> 2
+    num |= num >> 4
+    num |= num >> 8
+    num |= num >> 16
+    return self.x ^ num
+
+# Python 2
+
+class Solution(object):
+  def findComplement(self, num):
+    """
+    :type num: int
+    :rtype: int
+    """
     self.x = num
     num |= num >> 1
     num |= num >> 2
