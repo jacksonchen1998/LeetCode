7ea5ff6101b58c2eb8a391962df585116907b429
jacksonchen1998 <present90308@gmail.com>
2023-02-15 13:10:26 +0800

    [NEW] 141

---
+++ 141. Linked List Cycle/try.py
@@ -0,0 +1,11 @@
+class Solution():
+    def hasCycle(self, head):
+        try:
+            slow = head
+            fast = head.next
+            while slow != fast:
+                slow = slow.next
+                fast = fast.next.next
+            return True
+        except:
+            return False
